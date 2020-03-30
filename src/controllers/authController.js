8cc73faabfab713b8fd955e14eb607c875d3a117
Yuri Duarte <yuriduartedev@gmail.com>
2020-03-30 01:41:02 -0300

    Adiciona autenticação com JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
 
 const User = require('../models/User');
 
 const router = express.Router();
+
+function generateTotken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,19 +20,43 @@ router.post('/register', async (req, res) => {
   try {
     if (await User.findOne({ email }))
       return res.status(400).send({ error: 'User already exists' });
-    if (name.length < 1) 
+    if (name.length < 1)
       return res.status(400).send({ error: 'Name cannot be Empty' });
-    if (password.length < 1) 
+    if (password.length < 1)
       return res.status(400).send({ error: 'Password cannot be Empty' });
 
     const user = await User.create(req.body)
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({
+      user,
+      token: generateTotken({ id: user.id })
+    });
   } catch (err) {
     return res.status(400).send({ error: 'Registration failed' });
   }
+});
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');;
+
+  if(!user)
+    return res.status(400).send({ erro: 'User not found' });
+
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Invalid password' })
+
+  user.password = undefined;
+
+  const token =
+
+  res.send({
+    user,
+    token: generateTotken({ id: user.id })
+  });
 });
 
 module.exports = app => app.use('/auth', router);
